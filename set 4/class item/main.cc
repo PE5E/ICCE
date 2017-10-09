778e18cc1d33fbe30e359aa6c4f7f88008826533
Jaap van der Leest <jaapvanderleest@hotmail.com>
2017-10-09 17:29:53 +0200

    updated class item and makefile

--- set 4/class item/main.cc
+++ set 4/class item/main.cc
@@ -1,6 +1,4 @@
-#include <iostream>
-#include <string>
-#include "item.h"
+#include "myheader.ih"
 
 using namespace std;
 
@@ -17,42 +15,7 @@ int main()
 
     first.printItem();
 
-
-    size_t itemInputNr = 0;
-    string itemInputName = "";
-    string inputStr = "";
-
-    while (true)
-    {
-        itemInputName = "inputNr" + to_string(itemInputNr);
-        Item itemInputName;
-        inputStr = "";
-
-        cout << "Name of item: \n";
-        cin >> inputStr;
-        itemInputName.setName(inputStr);
-        
-        cout << "Category of item: \n";
-        cin >> inputStr;
-        itemInputName.setCategory(inputStr);
-
-        cout << "Number of item: \n";
-        cin >> inputStr;
-        itemInputName.setItemNr(inputStr);
-
-        cout << "Price of item: \n";
-        cin >> inputStr;
-        itemInputName.setPrice(stod(inputStr));
-
-        cout << "\n -------- \n\n" << "inputNr" + to_string(itemInputNr) << '\n';
-        itemInputName.printItem();
-
-        ++itemInputNr;
-
-        cout << "\n -------- \n\n";
-    }
-
-
+    inputToItem();
 
 }
 

cd2d49c506abb8d11ecf5e9b80fa7f2ddc0c0d59
Jaap van der Leest <jaapvanderleest@hotmail.com>
2017-10-10 20:28:24 +0200

    include commonheader.ih in memory.ih

--- set 4/virtualcpu/memory/memory.h
+++ set 4/virtualcpu/memory/memory.h
@@ -1,7 +1,9 @@
 #ifndef INCLUDED_MEMORY_
 #define INCLUDED_MEMORY_
 
-#include "../commonheader.ih"              // Includes enums
+// #include "../commonheader.ih"              // Includes enums
+// this should be in memory.ih. corrected it for you ;)
+
 
 class Memory
 {

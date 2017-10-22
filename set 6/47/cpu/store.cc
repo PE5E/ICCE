a6057926b64e67a9f08e8ac44c660eb14e4497d7
TS van der Berg <tmnvanderberg@gmail.com>
2017-10-22 17:10:16 +0000

    @_@

--- set 6/47/cpu/store.cc
+++ set 6/47/cpu/store.cc
@@ -21,23 +21,33 @@ void CPU::store(Operand const &lhs, int value)
 // lhs is struct 'Operand'
 // lhs.type is 'Operandtype' from enums.h  
 // code below is rewrite using function pointers
-
-void CPU::store(Operand const &lhs, int value)                                                      // should this be moved 1 lvl up? I think not.
+// enum class OperandType
+//    {
+//        SYNTAX,             // syntax error while specifying an operand
+//        VALUE,              // direct value
+//        REGISTER,           // register index
+//        MEMORY              // memory location (= index)
+//    };
+//
+void CPU::store(Operand const &lhs, int value)                                                  // should this be moved 1 lvl up? I think not.
 {
-    storeValue(lhs.value, value);                                                                   // store
+    storeValue[lhs.type](lhs.value, value);                                                     // store
 }
 
-void (*storeValue)(int place, int value) = lhs.type == OperandType::Register ? &storeRegister : // memory or register?
-                                           lhs.type == OperandType::Memory ? &storeMemory : 
-                                                                            nullptr;
+void (*storeValue[])(int place, int value)
+{
+    nullptr,
+    nullptr,
+    &storeRegister,
+    &storeMemory
+}
 
-
-void CPU::storeRegister(int place, int value)                                                       // sep file, add  to header
+void CPU::storeRegister(int place, int value)                                                   // sep file, add  to header
 {
     d_register[place] = value;
 }
 
-void CPU::storeMemory(int place, int value)                                                         // sep file, add to header
+void CPU::storeMemory(int place, int value)                                                     // sep file, add to header
 {
     d_memory.store(place, value);
 }

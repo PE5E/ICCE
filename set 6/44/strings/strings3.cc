0ce914ae6d84cab37aa4d6a3802398633c1186ec
Jaap van der Leest <jaapvanderleest@hotmail.com>
2017-10-25 14:39:00 +0200

    44 finished

--- set 6/44/strings/strings3.cc
+++ set 6/44/strings/strings3.cc
@@ -4,7 +4,7 @@ Strings::Strings(char *environLike[])
 :
     d_size(count(environLike)),
     d_capacity(d_size),
-    d_arrayStr(new string *[d_capacity]) 
+    d_arrayStr(rawPointers(d_capacity)) 
 
 {
     fill(environLike);       // fill the newly created array 

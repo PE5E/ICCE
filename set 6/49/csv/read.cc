332ff379170aa4771dbadc5ec23b45ed1ac0648a
TS van der Berg <tmnvanderberg@gmail.com>
2017-10-25 11:03:54 +0000

    doei map

--- set 6/49/csv/read.cc
+++ set 6/49/csv/read.cc
@@ -7,3 +7,9 @@ size_t CSV::read(std::istream &in, size_t nLines = 0)
             read1(d_lastLine);
             ++count;
 }
+
+// By default, all lines of in are read and are processed by the read1 member.
+// By specifying a non-zero value for the nLines parameter the specified number of
+// lines is read from in. Reading stops once in's status is not good. When nLines 
+// is specified as zero, then reading continues until all CSV lines have been processed.
+// The number of successfully processed lines is returned. 

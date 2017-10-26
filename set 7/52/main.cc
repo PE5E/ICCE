cb5334586d5796f08efb52d57b0ca999ca2dc4c7
tmnvanderberg@gmail.com <tmnvanderberg@gmail.com>
2017-10-26 21:24:06 +0200

    remake

---
+++ set 7/52/main.cc
@@ -0,0 +1,12 @@
+#include "iosfwd"
+
+// maybe write a declaration that tells the compiler to insert the arguments 
+// of fun in some ostream object?
+
+int main()
+{
+    fun();
+    fun("with functions");
+    fun(1, 2, 3);
+}
+        

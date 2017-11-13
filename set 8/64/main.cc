c2ae201e237ec53fe24d17e761ac22e84349f6de
TS van der Berg <tmnvanderberg@gmail.com>
2017-11-13 18:50:25 +0000

    lol

---
+++ set 8/64/main.cc
@@ -0,0 +1,7 @@
+#include "matrix/matrix.h"
+
+int main()
+{
+    Matrix mat();
+    Matrix mati(3,4);
+}
---
+++ set 8/64/matrix/matrix4.cc
@@ -0,0 +1,9 @@
+#include "matrix.ih"
+
+Matrix::Matrix(size_t ncols, size_t nrows)
+    :
+        d_nrows(nrows),
+        d_ncols(ncols)
+{
+    d_data = new double[ncols * nrows]();       // () guarantees initial 0
+}

40c662065d9cc0ef0b210540bbc57fcc1ec9cb6a
tmnvanderberg@gmail.com <tmnvanderberg@gmail.com>
2017-11-15 01:06:30 +0100

    -

--- set 8/64/matrix/matrix5.cc
+++ set 8/64/matrix/matrix5.cc
@@ -5,10 +5,11 @@ Matrix::Matrix(std::initializer_list<std::initializer_list<double>> rows)
         d_nrows(rows.size()),
         d_ncols(rows.begin()->size())
 {   
-    d_data = new double[d_nrows * d_ncols];         // alloc
-    for (size_t rowidx = 0; rowidx != d_nrows; ++ rowidx)   // loop rows
+    d_data = new double[d_nrows * d_ncols];                  // alloc
+
+    for (size_t rowidx = 0; rowidx != d_nrows; ++ rowidx)    // loop rows
     {
-        double const *dptr = rows.begin() + rowidx; // first 
+        double const *dptr = rows.begin() + rowidx;          // first 
 
         if (dptr->size() != d_ncols)
         {

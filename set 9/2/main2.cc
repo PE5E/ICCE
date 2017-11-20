b7761920bfe638130e22223c56b7e11df5c0bcba
Jaap van der Leest <jaapvanderleest@hotmail.com>
2017-11-20 17:57:15 +0100

    opdracht 2

---
+++ set 9/2/main2.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+bool promptGet(istream &in, string &str)
+{
+    cout << "Enter a line or ^D\n";     // ^D signals end-of-input
+
+    return getline(in, str);
+}
+        
+void process(string &str)
+{
+    cout << "This is your input: \n" << str << '\n';
+}
+
+int main()
+{
+    string str;
+
+    while (promptGet(cin, str))
+        process(str);
+}
+        
+

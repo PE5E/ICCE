64bb866fed63fb249bbdc604ea74bdfe21d54231
TS van der Berg <tmnvanderberg@gmail.com>
2017-11-21 14:04:22 +0000

    3 finished

--- part_II/set_1/3/main.cc
+++ part_II/set_1/3/main.cc
@@ -1,8 +1,16 @@
 #include "main.ih"
+#include <iostream>
+#include <cstring>      // memcpy
+using std::memcpy;
 
 int main(int argc, char **argv)
 {
-    Matrix mat{4, 6};
-    show(mat);
+    Matrix mat1{4, 6};
+    Matrix mat2 = mat1.identity(5);
+    show(std::cout, mat2);
+    std::cout << "-----" << '\n';
+    memcpy(mat2[2], mat2[1], 5 * sizeof(double));
+    mat2[2][3] = 23.5;
+    show(std::cout, mat2);
 }
 

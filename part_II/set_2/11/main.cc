7e2a49e2f6bdd96c096591c2e1d953d6e51d2f61
tmnvanderberg@gmail.com <tmnvanderberg@gmail.com>
2017-11-24 09:11:24 +0100

    fine

--- part_II/set_2/11/main.cc
+++ part_II/set_2/11/main.cc
@@ -13,7 +13,9 @@ int main()
     pl();
     try
     {
+        std::cerr << "Trying" << '\n';
         Dem lol;
+        std::cerr << "Throwing" << '\n';
         throw lol;
     }
     catch(Dem lol)
@@ -26,12 +28,62 @@ int main()
     pl();
     try
     {
-        Dem lol;
-        throw lol;
+        std::cerr << "Trying" << '\n';
+        Dem lol2;
+        std::cerr << "Throwing" << '\n';
+        throw lol2;
     }
     catch(Dem &lol)
     {
         std::cerr << "Catching!" << '\n';
+    }
+    pl();
+
+    std::cerr << "Example 3: rethrow by reference" << '\n';
+    pl();
+    try
+    {
+        std::cerr << "Trying (outer)" << '\n';
+    try
+    {
+        std::cerr << "Trying (inner)" << '\n';
+        Dem lol3;
+        std::cerr << "Throwing" << '\n';
+        throw lol3;
+    }
+    catch(Dem &lol)
+    {
+        std::cerr << "Catching!" << '\n';
+        std::cerr << "Re-throwing!" << '\n';
+        throw;
+    }
+    }
+    catch(Dem &lol)
+    {
+        std::cerr << "Re-catching!" << '\n';
+    }
+    pl();
+
+    std::cerr << "Example 4: rethrow object" << '\n';
+    pl();
+    try
+    {
+        std::cerr << "Trying (outer)" << '\n';
+    try
+    {
+        std::cerr << "Trying (inner)" << '\n';
+        Dem lol4;
+        throw lol4;
+    }
+    catch(Dem lol)
+    {
+        std::cerr << "Catching!" << '\n';
+        throw;
+    }
+    }
+    catch(Dem lol)
+    {
+        std::cerr << "Re-catching!" << '\n';
     }
     pl();
 }

c15dc50ff19baa9e4651717796ce839f924c1032
tmnvanderberg@gmail.com <tmnvanderberg@gmail.com>
2017-12-11 17:11:21 +0100

    working

---
+++ part_II/set_4/25/bistream/bistream.h
@@ -0,0 +1,47 @@
+#ifndef __INCLUDED_BISTREAM_H
+#define __INCLUDED_BISTREAM_H
+
+#include <iostream>
+#include <ios>
+
+class BiStream: public std::ostream
+{
+    class BiStreamBuffer: public std::streambuf
+    {
+        std::streambuf *d_buffer1;
+        std::streambuf *d_buffer2;
+        public:
+
+            BiStreamBuffer(std::streambuf *one, std::streambuf *two)
+                :
+                    d_buffer1(one),
+                    d_buffer2(two)
+            {
+            }
+
+            virtual int overflow(int c)
+            {
+                d_buffer1->sputc(c);
+                d_buffer2->sputc(c);
+                return c;
+            }
+
+        private:
+    };
+
+    BiStreamBuffer d_buffer;
+
+    public:
+
+        BiStream(std::ostream &one, std::ostream &two)
+            :
+                d_buffer(one.rdbuf(), two.rdbuf())         // construct
+        {
+            std::ostream::rdbuf(&d_buffer);                // associate 
+        }
+
+
+    private:
+};
+
+#endif

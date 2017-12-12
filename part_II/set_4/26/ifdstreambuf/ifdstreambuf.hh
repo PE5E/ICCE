b68337337d9064e3b896a9d8c7a7ba7225f796d6
tmnvanderberg@gmail.com <tmnvanderberg@gmail.com>
2017-12-12 16:13:00 +0100

    26 fine

---
+++ part_II/set_4/26/ifdstreambuf/ifdstreambuf.hh
@@ -0,0 +1,40 @@
+#ifndef __INCLUDED_IFDSTREAMBUF_H
+#define __INCLUDED_IFDSTREAMBUF_H
+
+#include <streambuf>
+
+class IFdStreambuf: public std::streambuf
+{
+    public:
+        enum Mode
+        {
+            CLOSE_FD,
+            KEEP_FD,
+        };
+    private:
+        static const size_t s_SIZE = 100; 
+
+        char  *d_buffer;
+        int    d_fd;
+        Mode   d_mode;
+    public:
+        explicit IFdStreambuf(Mode mode = KEEP_FD);
+        explicit IFdStreambuf(int fd, Mode mode = KEEP_FD);
+
+        ~IFdStreambuf();
+
+        IFdStreambuf()                                      = delete;
+        IFdStreambuf(IFdStreambuf const &other)             = delete;
+        IFdStreambuf &operator=(IFdStreambuf const &other)  = delete;
+
+        void open(int fd, Mode mode = KEEP_FD);
+        void close(); 
+    private:
+        std::streamsize xsgetn(char *file, std::streamsize requestSize);
+
+        void clean();
+
+}; // IFdstreambuf
+
+#endif
+
---
+++ part_II/set_4/26/main.cc
@@ -0,0 +1,6 @@
+#include "ifdstreambuf/ifdstreambuf.hh"
+
+int main()
+{
+    IFdStreambuf object(IFdStreambuf KEEP_FD);
+}

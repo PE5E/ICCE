4d614b3e07551b51b0c4860e5ad1d7b8528d505a
TS van der Berg <tmnvanderberg@gmail.com>
2018-01-12 23:44:11 +0000

    dingen

---
+++ part_II/set_6/44/vstring/count.cc
@@ -0,0 +1,27 @@
+#include "vstring.ih"
+
+Vstring::count(std::string const &str,
+               CharMap &cmap,
+               bool (*accept)(char, CharMap &))
+{
+    std::for_each(str.begin(), 
+                  str.end(), 
+                  [&] (char ch) 
+                    {
+                        return accept(ch, cmap);     
+                    } );
+
+}
+// Implement count using for_each and a lambda function. It
+// must visit each of the strings stored in Vstring, returning the total
+// number of accepted characters, storing the frequencies of accepted characters
+// in its cmap parameter. It may be assumed that cmap initially refers to
+// an empty map.
+//
+// The for_each algorithm cannot directly be used i
+// (i.e., by passing *this as the function object argument) 
+// inside a member function to modify its own object as the for_each
+// algorithm first creates its own copy of the passed function object.
+// A lambda function or a wrapper class whose constructor accepts a
+// pointer or reference to the current object and possibly to one of
+// its member functions solves this problem. 

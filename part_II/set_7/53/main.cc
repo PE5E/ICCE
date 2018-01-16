32e11124895ec70aeb526921cf68f5f6e5c944a5
Jaap van der Leest <jaapvanderleest@hotmail.com>
2018-01-17 00:02:13 +0100

    53 progress

---
+++ part_II/set_7/53/main.cc
@@ -0,0 +1,13 @@
+#include "storage/storage.h"
+
+using namespace std;
+
+int main()
+{
+    Storage stor;
+
+
+
+
+}
+
--- part_II/set_7/53/storage/storage.h
+++ part_II/set_7/53/storage/storage.h
@@ -1,30 +1,65 @@
 #ifndef INCLUDED_STORAGE_
 #define INCLUDED_STORAGE_
 
+#include <mutex>
+#include <queue>
+#include <string>
 
 class Storage
 {
-    queue d_queue;
-    bool  d_finished = false;
+    std::mutex              d_mutex;
+    std::queue<std::string> d_queue;
+    bool                    d_finished = false;
     
     public:
         Storage();
 
-        void push();
-        void pop();
-        void front();
-        bool empty();
-        void finished(bool finish); // also possible: void finished()
+        void        push(std::string input);
+        void        pop();
+        std::string front();
+        bool        empty();
+        void        finished(bool finish); // also possible: void finished()
                                     // but this way it is possible to re-open
 
     private:
-        bool finished();
+        bool finished();            // check if finished is set
 };
 
-inline void finished(bool finish)
+inline void Storage::push(std::string input)
+{
+    std::lock_guard<mutex> lg(d_mutex);
+    this->push(input);
+    return;
+}
+
+inline void Storage::pop()
+{
+    std::lock_guard<mutex> lg(d_mutex);
+    this->pop();
+    return;
+}
+
+inline std::string Storage::front()
+{
+    std::lock_guard<mutex> lg(d_mutex);
+    return this->front();
+}
+
+inline bool Storage::empty()
+{
+    std::lock_guard<mutex> lg(d_mutex);
+    return d_queue.empty() && d_finished;
+}
+
+inline void Storage::finished(bool finish)
 {
     d_finished = finish;
     return;
+}
+
+inline bool Storage::finished()
+{
+    return d_finished;
 }
         
 #endif

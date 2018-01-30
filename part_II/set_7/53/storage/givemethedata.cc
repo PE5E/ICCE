82ce7e31cdeece6553ceb6e8c34af33e843ddafd
Jaap van der Leest <jaapvanderleest@hotmail.com>
2018-01-30 21:55:12 +0100

    53 catching

--- part_II/set_7/53/storage/givemethedata.cc
+++ part_II/set_7/53/storage/givemethedata.cc
@@ -5,10 +5,18 @@ bool Storage::giveMeTheData()
     std::lock_guard<mutex> lg(d_mutex);
     if (d_queue.empty())
         return false;
+    
+    try
+    {
+        d_returnStr = move(d_queue.front());
+    }
 
-    if (d_returnStr = move(d_queue.front()));
-        d_queue.pop();
+    catch (...)
+    {
+        return false;
+    }    
 
+    d_queue.pop();
     return true;
 }
 

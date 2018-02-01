c3aa5168d50d304b5bb1ec8a8c4cfa20da211f2c
Rob Hermsen <rob.hermsen@live.nl>
2018-02-01 14:08:11 +0100

    60 done

---
+++ part_II/set_8/60/main.cc
@@ -0,0 +1,13 @@
+#include "main.h"
+
+int main()
+{
+    int ia[20]= {20, 5, 1, 64, 700, 8, 100, 7, 81, 1, 124, 1, 45, 
+		6, 15, 2, 34, 1, 18, 20};
+    quicksort(ia, ia + 20);
+	
+    for (size_t idx = 0; idx < 20; ++idx)
+        cout << ia[idx] << ' ';
+
+    cout << '\n';
+}
---
+++ part_II/set_8/60/main.h
@@ -0,0 +1,12 @@
+#ifndef INCLUDED_MAIN_
+#define INCLUDED_MAIN_
+
+#include <future>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+void quicksort(int *beg, int *end);
+
+#endif

d5939dbfbf35ba745687d0e69e6e9b4b70f054a7
DaanLamb <daan.lambert@gmail.com>
2018-03-06 16:03:47 +0100

    finished ex16

--- part_III/set_3/16/main.cc
+++ part_III/set_3/16/main.cc
@@ -2,5 +2,12 @@
 
 int main()
 {
-
+    cout <<
+        Type<int>::located << ' ' << 
+        Type<int, double>::located << ' ' <<
+        Type<int, int>::located << ' ' <<
+        Type<int, double, int>::located << ' ' <<
+        Type<int, double, int>::located << ' ' <<
+        Type<int, double, int, int, int>::located <<
+        '\n';
 }

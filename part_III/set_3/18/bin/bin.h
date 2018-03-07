1bf886c562e5428d84ae990a9d7011918d0172b7
DaanLamb <daan.lambert@gmail.com>
2018-03-07 13:59:01 +0100

    changed ex18

--- part_III/set_3/18/bin/bin.h
+++ part_III/set_3/18/bin/bin.h
@@ -1,16 +1,16 @@
 #ifndef INCLUDED_BIN_
 #define INCLUDED_BIN_
 
-#include <bitset>
-#include <cmath>
-
 template <size_t x>
 struct Bin
 {
-    static constexpr std::bitset<static_cast<int>(log(x) / log(2)) + 1> 
-    const value = 
-    {std::bitset<static_cast<int>(log(x) / log(2)) + 1>(x)};
+    enum {value = (Bin<(x >> 1)>::value * 10) + (x - (x >> 1 << 1))};
+};
+
+template <>
+struct Bin<0>
+{
+    enum {value = 0};
 };
 
 #endif    // INCLUDED_BIN_
-

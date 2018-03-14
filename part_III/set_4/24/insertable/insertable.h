8a913dcf45cc8febfe596297fa8c550032f8dace
DaanLamb <daan.lambert@gmail.com>
2018-03-14 15:12:59 +0100

    improved 24 and 25

--- part_III/set_4/24/insertable/insertable.h
+++ part_III/set_4/24/insertable/insertable.h
@@ -1,26 +1,44 @@
 #ifndef INCLUDED_INSERTABLE_
 #define INCLUDED_INSERTABLE_
 
-#include <ostream>
+#include <iosfwd>
+
+template <template <typename> class Container, typename Data>
+class Insertable;
+
+template <template <typename> class Container, typename Data>
+std::ostream& operator<<(std::ostream& out,
+                            Insertable<Container, Data> const &insertable);
 
 template <template <typename> class Container, typename Data>
 class Insertable: public Container<Data>
 {
+    friend std::ostream& operator<< <>(std::ostream& out,
+                                    Insertable<Container, Data> const &insertable);
     public:
-        Insertable() = default;
         Insertable(Container<Data> container);
-        Insertable(Insertable<Container, Data> const &other);
-        Insertable(Insertable<Container, Data> &&tmp);
-        Insertable(Data &&data);
 
-        void insertIntoStream(std::ostream& out) const;
+        //Constructor Insertable(Data &&data) is available via the Container
+        //constructors. For for example vector this however makes an vector of
+        //zeroes with size data. If you want a vector with as only element data,
+        //uncomment the next line and the include below.
+        //Insertable(Data &&data);
+
+        using Container<Data>::Container;
+    private:
+        std::ostream& insertIntoStream(std::ostream& out) const;
 };
 
 template <template <typename> class Container, typename Data>
-inline std::ostream& operator<<(std::ostream& out,
-                                Insertable<Container, Data> const &insertable)
+std::ostream& operator<<(std::ostream& out,
+                         Insertable<Container, Data> const &insertable)
 {
-    insertable.insertIntoStream(out);
-    return out;
+    return insertable.insertIntoStream(out);
 }
+
+#include "insertable1.ff"
+//uncomment the following line for the extra constructor
+//#include "insertable2.ff"
+#include "insertintostream.ff"
+
 #endif
